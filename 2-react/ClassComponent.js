e31c7f047a44655004b4de5dc63f0ef5361dcd19
utumenbayar-miu <utumenbayar@miu.edu>
2021-08-31 15:52:47 -0500

    Cutting branch for lesson 2

--- 2-react/ClassComponent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class ClassComponent extends React.Component {
-  render() {
-    return <h3>Hello from Class Component</h3>;
-  }
-}
-
-export default ClassComponent;
--- 2-react/FunctionalComponent.js
+++
@@ -1,3 +0,0 @@
-export default function FunctionalComponent() {
-  return <h3>Hello from Functional Component</h3>;
-}
